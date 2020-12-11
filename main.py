281df07c8c45e6ffc2c2e3d766f34cdba18f04a2
migantoju <a_niupi_byy@hotmail.com>
2020-12-10 22:09:51 -0600

    [Added .github workflows]

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 import os
-from dataclasses import dataclass
-from typing import Dict
-
-import requests
 
 GITHUB_REPOSITORY = os.getenv("GITHUB_REPOSITORY")
 GITHUB_REPOSITORY_OWNER = os.getenv("GITHUB_REPOSITORY_OWNER")
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+REVIEWERS = os.getenv("INPUT_REVIEWERS")
 
 GITHUB_API_URL = "https://api.github.com"
 GITHUB_HEADERS = {"Accept": "application/vnd.github.v3+json"}
 
 print(GITHUB_REPOSITORY)
 print(GITHUB_REPOSITORY_OWNER)
-print(GITHUB_TOKEN[:5])
-
+print(GITHUB_TOKEN[:3])
+print(REVIEWERS)
